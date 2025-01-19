d726318c123a17023b9f6876d274db440e644d64
MiikaM <miika.mikkonen5@gmail.com>
2025-01-19 11:31:46 +0200

    Initialize

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react'
+import Filter from './components/Filter'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
+import Notification from './components/Notification'
+
+
+const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(initializeAnecdotes())
+  }, [dispatch])
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <Notification />
+      <Filter />
+      <AnecdoteList />
+      <h2>create a new anecdote</h2>
+      <AnecdoteForm />
+    </div>
+  )
+}
+
+export default App
---
+++ src/reducers/anecdoteReducer.js
@@ -0,0 +1,54 @@
+import anecdoteService from '../services/anecdotes'
+
+export const createAnecdote = (content) => {
+  return async dispatch => {
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch({
+      type: 'NEW_ANECDOTE',
+      data: newAnecdote
+    })
+  }
+}
+
+export const vote = (anecdote) => {
+  const copy = { ...anecdote, votes: anecdote.votes + 1 }
+  return async dispatch => {
+    const updateObject = await anecdoteService.updateObject(copy)
+    dispatch({
+      type: 'VOTE',
+      data: updateObject
+    })
+
+  }
+}
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch({
+      type: 'INIT_ANECDOTES',
+      data: anecdotes
+    })
+  }
+}
+
+const reducer = (state = [], action) => {
+
+  switch (action.type) {
+    case 'NEW_ANECDOTE':
+      return state.concat(action.data)
+    case 'VOTE':
+      const id = action.data.id
+      return state.map(anecdote =>
+        anecdote.id !== id ? anecdote : action.data
+      )
+    case 'INIT_ANECDOTES':
+      return action.data
+    default:
+      return state
+  }
+
+
+}
+
+export default reducer
